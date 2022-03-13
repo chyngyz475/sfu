6919fe3708833448c2002a11888568ef69624047
chyngyz475 <61840750+chyngyz475@users.noreply.github.com>
2022-03-14 03:54:00 +0700

    1
    1

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,20 @@
 from csv import list_dialects
 from django.contrib import admin
-from.models import News, Post, Comment
-
+from.models import News, Post, Comment,Category,Tag
+from mptt.admin import MPTTModelAdmin
+from .import models
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["title", "create_at", "id"]
+    list_display = ["name","category", "create_at", "id"]
 
 
 
 # Register your models here.
-admin.site.register(News)
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(models.News, MPTTModelAdmin)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
 
 
--- news/models.py
+++ news/models.py
@@ -1,26 +1,75 @@
+from optparse import Option
+from re import T
+import time
 from turtle import title
 from venv import create
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
 
 class News(models.Model):
    title = models.CharField(max_length=250)
-   content = models.TextField(max_length=500)
    cteated_at = models.DateField(auto_now_add=True)
    photo = models.ImageField(upload_to='%Y/%m/%d')
+   content = models.TextField(max_length=500)
    
    
    def __str__(self):
       return
 
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    parent = TreeForeignKey(
+        'self',
+        related_name="children",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+      author = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
       title = models.CharField(max_length=200)
       image = models.ImageField(upload_to='articles/')
       text = models.TextField()
+      category = models.ForeignKey(
+            Category,
+            related_name="post",
+            on_delete=models.SET_NULL,
+            null=True 
+            )
       create_at = models.DateTimeField(auto_now_add=True)
       
 class Comment(models.Model):
        name = models.CharField(max_length=50)
        email = models.CharField(max_length=150)
-       website = models.CharField(max_length=150)
+       website = models.CharField(max_length=150, blank=True, null=True )
        message = models.TextField(max_length=500)
+       create_at = models.DateTimeField(auto_now_add=True)
+       post = models.ForeignKey(
+             Post,
+             related_name="comment",
+             on_delete=models.CASCADE,
+             null=True,
+             blank=True
+       )
        
